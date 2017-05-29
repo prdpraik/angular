4d92c714bcb56b558472bc3d39c15c2cbe52b1ee
nicolewim <nicolewimlaf@gmail.com>
2017-05-29 20:27:34 +0530

    Add existing file

--- demo/test.php
+++ demo/test.php
@@ -1,3 +1,12 @@
 <?php
-print_r($_REQUEST);
+ // $postdata = file_get_contents("php://input");
+  // print_r($postdata);  
+ //$request = json_decode($postdata);
+   // @$email = $request->pwd;
+   // @$pass = $request->pass;
+//    echo $email;
+  // echo  $pass = $request->pass;
+//print_r($_REQUEST);
+echo json_encode(array('result'=>1));
+
 ?>
