0c07a070015aec3c3aa1cac0175ccf6384886c7d
nicolewim <nicolewimlaf@gmail.com>
2017-06-07 19:50:32 +0530

    07062017

---
+++ angular2-demo/app/hello_world_app.component.ts
@@ -0,0 +1,13 @@
+import {Component, View} from "angular2/core";
+
+@Component({
+   selector: 'my-app1'
+})
+
+@View({
+  template: '<h2>Hello World !!</h2>'
+})
+
+export class MyHelloWorldClass {
+
+}
